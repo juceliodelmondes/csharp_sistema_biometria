89d4789bbd3e57b590d1d2b4dea3900536da4640
Jucelio Delmondes <jucelio.2013@hotmail.com>
2020-09-11 03:25:49 -0300

    first commit

---
+++ BiometriaArduinoConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace BiometriaArduinoConsole {
+    class Program
+    {
+        static SerialPort serial = new SerialPort();
+        static void Main(string[] args)
+        {
+            serial.PortName = "COM6";
+            serial.BaudRate = 9600;
+            serial.Open();
+            serial.DataReceived += new SerialDataReceivedEventHandler(ler);
+            while(true)
+            {
+                String cmd = Console.ReadLine();
+                if(cmd.Equals("exit"))
+                {
+                    Environment.Exit(1);
+                } 
+                else
+                {
+                    serial.Write(cmd);
+                }
+            }
+        }
+
+        private static void ler(Object sender, EventArgs e)
+        {
+            Console.WriteLine(serial.ReadLine());
+        }
+    }
+}
